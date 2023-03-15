4a4ead235a0989f2dd0a306c8cfacecfc5daab2e
Havoc <shivanshsingh9670@gmail.com>
2023-03-15 15:36:27 +0530

    Print Hey

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ExampleProj
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Debug.WriteLine("Hey");
+        }
+    }
+}
